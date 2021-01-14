aa49df8128ad492e56e174217b6a425bbea45ea3
AJthebeast <75455189+AJthebeast@users.noreply.github.com>
2021-01-13 17:54:19 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var canvas=new.canvas("myCanvas")
+if (KeyPressEvent=="70");
+{
+    new-image(ironman_face.png);
+    console.log("f");
+}
+if (KeyPressEvent=="66");
+{
+    new-image(spiderman_body.png);
+    console.log("v");
+}
+if (KeyPressEvent=="76");
+{
+    new-image(hulk_legs.png);
+    console.log("l");
+}
+if (KeyPressEvent=="82");
+{
+    new-image(thor_right_hand.png);
+    console.log("r")
+}
+if (KeyPressEvent=="72");
+{
+    newz-image(captain_america_left_hand.png);
+    console.log("h")
+}
